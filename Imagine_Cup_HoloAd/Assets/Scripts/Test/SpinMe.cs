b0c7febf5bc4c3b159c6da35918569b145df10ba
Xyzzyall <xyzzyall@gmail.com>
2018-03-15 20:43:20 +0300

    Testing GitHub
    Test

---
+++ Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMe : MonoBehaviour {
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0, speed, 0);
+	}
+}
