9cd96f276962d47dc4e5a916d8416708f6cc0ac1
Xyzzyall <xyzzyall@gmail.com>
2018-03-15 20:57:11 +0300

    Test 4

--- Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
+++ Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
@@ -9,14 +9,11 @@ public class SpinMe : MonoBehaviour {
     private float time = 0f;
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-        time += (float)System.Math.PI/speed;
+        time += Time.deltaTime*(float)System.Math.PI/speed;
         transform.Rotate(0, speed, 0);
         transform.Translate(0, range * (float)System.Math.Sin(time), 0);
-=======
         gameObject.transform.Rotate(0, speed, 0);
         transform.Translate(new Vector3(0,0,1*Time.deltaTime));
         //this is awesome hehe
->>>>>>> b093991cb12c0ed190c1f3f44081d49b6ed820b1
 	}
 }
