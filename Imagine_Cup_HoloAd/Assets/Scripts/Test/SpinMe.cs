b093991cb12c0ed190c1f3f44081d49b6ed820b1
DESKTOP-9E8U56S\Anton Pushkin <anton.pushkin2@gmail.com>
2018-03-15 20:49:41 +0300

    script

--- Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
+++ Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
@@ -8,5 +8,7 @@ public class SpinMe : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         gameObject.transform.Rotate(0, speed, 0);
+        transform.Translate(new Vector3(0,0,1*Time.deltaTime));
+        //this is awesome hehe
 	}
 }
