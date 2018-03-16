77202eef9a8f9d40eea11cb8d154d5210ea9734b
DESKTOP-9E8U56S\Anton Pushkin <anton.pushkin2@gmail.com>
2018-03-16 14:16:06 +0300

    more

--- Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
+++ Imagine_Cup_HoloAd/Assets/Scripts/Test/SpinMe.cs
@@ -15,5 +15,6 @@ public class SpinMe : MonoBehaviour {
         gameObject.transform.Rotate(0, speed, 0);
         transform.Translate(new Vector3(0,0,1*Time.deltaTime));
         //this is awesome hehe
+        //this is a new line
 	}
 }
